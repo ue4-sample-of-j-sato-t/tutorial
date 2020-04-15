842ab591d305533bb5ec17a8f478d400971d4b54
JunS <jmob0706unpc@live.jp>
2020-04-15 16:27:48 +0900

    [add] ゲーム制御のカメラ操作まで終了

---
+++ Source/UE4_Tutorial/CameraDirector.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CameraDirector.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ACameraDirector::ACameraDirector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACameraDirector::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACameraDirector::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const float TimeBetweenCameraChange = 2.0f;
+	const float SmoothBlendTime = 0.75f;
+	
+	TimeToNextCameraChange -= DeltaTime;
+	if (TimeToNextCameraChange <= 0.0f)
+	{
+		TimeToNextCameraChange += TimeBetweenCameraChange;
+
+		// プレイヤーコントローラー取得
+		APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
+		if (OurPlayerController) // 取得できたか
+		{
+			// 次のカメラを指定
+			int32 BeforCameraIndex = NowCameraIndex;
+			NowCameraIndex = (NowCameraIndex + 1) % Cameras.Num();
+
+			// 切り替え先がある
+			if ((NowCameraIndex != BeforCameraIndex) && (Cameras[NowCameraIndex] != nullptr))
+			{
+				// 変数に保持
+				auto TargetCamera = Cameras[NowCameraIndex];
+
+				// インデックスが奇数のときは瞬時
+				if (NowCameraIndex % 2)
+				{
+					OurPlayerController->SetViewTarget(TargetCamera);
+				}
+				else  // 偶数ならスムース
+				{
+					OurPlayerController->SetViewTargetWithBlend(TargetCamera, SmoothBlendTime);
+				}
+			}
+		}
+	}
+}
+
---
+++ Source/UE4_Tutorial/CameraDirector.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CameraDirector.generated.h"
+
+UCLASS()
+class UE4_TUTORIAL_API ACameraDirector : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TArray<AActor*> Cameras;
+	
+	int32 NowCameraIndex;
+
+	float TimeToNextCameraChange;
+	
+public:	
+	// Sets default values for this actor's properties
+	ACameraDirector();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
