20bc8b1fdfa360ee49a4ce9451d9a9236f79de93
JunS <jmob0706unpc@live.jp>
2020-04-15 17:52:06 +0900

    [add] Pawnの操作についてまで終了

---
+++ Source/UE4_Tutorial/MyPawn.cpp
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawn.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// プレイヤー0に制御されるようにする
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	// ルートコンポーネント生成
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	// 表示用コンポーネント生成
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>("OurVisibleComponent");
+	OurVisibleComponent->SetupAttachment(RootComponent);
+
+	// カメラコンポーネント生成
+	auto OurCameraComponent = CreateDefaultSubobject<UCameraComponent>("OurCamera");
+	OurCameraComponent->SetupAttachment(RootComponent);
+	// 位置・角度調整
+	OurCameraComponent->SetRelativeLocation(FVector(-250.0f, 0.0f, 250.0f));
+	OurCameraComponent->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// スケール変更
+	float CurrentScale = OurVisibleComponent->GetComponentScale().X;
+	if (bGrowing)
+	{
+		CurrentScale += DeltaTime;
+	}
+	else
+	{
+		CurrentScale -= (DeltaTime * 0.5f);
+	}
+	// リミット
+	CurrentScale = FMath::Clamp(CurrentScale, 1.f, 2.f);
+	OurVisibleComponent->SetWorldScale3D(FVector(CurrentScale));
+
+	// 移動
+	if (!CurrentVelocity.IsZero())
+	{
+		FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+		SetActorLocation(NewLocation);
+	}
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Grow", IE_Pressed, this, &AMyPawn::StartGrowing);
+	PlayerInputComponent->BindAction("Grow", IE_Released, this, &AMyPawn::StopGrowing);
+
+	PlayerInputComponent->BindAxis("MoveX", this, &AMyPawn::Move_XAxis);
+	PlayerInputComponent->BindAxis("MoveY", this, &AMyPawn::Move_YAxis);
+}
+
+void AMyPawn::Move_XAxis(float AxisValue)
+{
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void AMyPawn::Move_YAxis(float AxisValue)
+{
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.f) * 100.f;
+}
+
+void AMyPawn::StartGrowing()
+{
+	bGrowing = true;
+}
+
+void AMyPawn::StopGrowing()
+{
+	bGrowing = false;
+}
+
---
+++ Source/UE4_Tutorial/MyPawn.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MyPawn.generated.h"
+
+UCLASS()
+class UE4_TUTORIAL_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+
+	// 表示用コンポーネント
+	UPROPERTY(EditAnywhere)
+	USceneComponent* OurVisibleComponent;
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//Input functions (入力関数)
+	void Move_XAxis(float AxisValue);
+	void Move_YAxis(float AxisValue);
+	void StartGrowing();
+	void StopGrowing();
+
+	//Input variables (入力変数)
+	FVector CurrentVelocity;
+	bool bGrowing;
+};
