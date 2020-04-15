5e43473e12e297e0705127b4ad7650dd922cf568
JunS <jmob0706unpc@live.jp>
2020-04-15 15:21:58 +0900

    [add] クイックスタート終了まで

---
+++ Source/UE4_Tutorial/FloatingActor.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingActor.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+AFloatingActor::AFloatingActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 基礎メッシュ
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+
+	VisualMesh->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_cube.Shape_cube"));
+
+	if (CubeVisualAsset.Succeeded())
+	{
+		VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
+		VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	}
+
+	// パーティクル
+	ParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticles"));
+	ParticleSystem->SetupAttachment(RootComponent);
+	ParticleSystem->bAutoActivate = false;
+	//ParticleSystem->SetRelativeLocation(FVector(30.0f, 0.0f, 0.0f));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if( ParticleAsset.Succeeded()){
+		ParticleSystem->SetTemplate(ParticleAsset.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AFloatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation();
+	FRotator NewRotator = GetActorRotation();
+
+	float RunningTime = GetGameTimeSinceCreation();
+	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+
+	NewLocation.Z += DeltaHeight * FloatSpeed;
+
+	float DeltaRotation = DeltaTime * RotationSpeed;
+	NewRotator.Yaw += DeltaRotation;
+
+	SetActorLocationAndRotation(NewLocation, NewRotator);
+
+	ParticleSystem->SetRelativeLocation(ParticlePosition);
+}
+
+void AFloatingActor::ToggleActive()
+{
+	ParticleSystem->ToggleActive();
+}
+
---
+++ Source/UE4_Tutorial/FloatingActor.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloatingActor.generated.h"
+
+UCLASS()
+class UE4_TUTORIAL_API AFloatingActor : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* VisualMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* ParticleSystem;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FloatingActor")
+	float FloatSpeed = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FloatingActor")
+	float RotationSpeed = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FloatingActor")
+	FVector ParticlePosition;
+
+
+public:	
+	// Sets default values for this actor's properties
+	AFloatingActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UFUNCTION(BlueprintCallable)
+	void ToggleActive();
+};
